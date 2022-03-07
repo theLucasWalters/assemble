5fc9d896a52fb9cb8b3dac20951603945c5b89f4
theLucasWalters <lwalters224@gmail.com>
2022-03-06 20:55:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Assemble Module
+I don't know if I can actually call this a module, but whatever.
+
+Basically, assembling Assembly files is hard on Windows. Now it's not. Have fun!
+
+## How I use this
+I used `pyinstaller` to compile the script into a `.exe` and added that to my path so I can use it wherever I want. Works like a charm!
+
+**This is meant for x64 Windows only*
---
+++ assemble.py
@@ -0,0 +1,46 @@
+# assemble to .obj: /masm32/bin/ml /c /Zd /coff filename.asm
+# link to .exe:     /masm32/bin/Link /SUBSYSTEM:CONSOLE filename.obj
+
+# imports
+import os
+import sys
+
+# variables to make things simpler for myself
+osys    = os.system
+sysargs = sys.argv
+
+# main
+def main():
+
+    # check that a file was actually input
+    try:
+        file = sysargs[1]
+    except:
+        # if not, throw an error
+        print("Enter the file you want to assemble.")
+        quit()
+
+    # call parse() to get the file name (fn) and the file extension (ext)
+    fn, ext = parse(file)
+
+    # it needs to be an assembly file, otherwise it won't work
+    if ext != 'asm':
+        print("The input file must be a '.asm' file.")
+        quit()
+
+    # define the commands
+    cmd1 = f'/masm32/bin/ml /c /Zd /coff {fn}.asm'
+    cmd2 = f'/masm32/bin/Link /SUBSYSTEM:CONSOLE {fn}.obj'
+
+    # call the commands
+    osys(cmd1)
+    osys(cmd2)
+
+# function to parse the file name at the '.'
+def parse(file):
+    fn, ext = file.split('.')
+    return fn, ext
+
+# because this is meant to run in the console
+if __name__ == "__main__":
+    main()
